376ea78a2b596600a7d4637901e0ec1f2409130e
Pallavi Pagoti <136489737+pallavipagoti@users.noreply.github.com>
2024-12-22 14:01:48 -0600

    Update InstaCloneApplication.java

--- src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
+++ src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
@@ -1,5 +1,5 @@
 package com.palla.Insta_Clone;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
