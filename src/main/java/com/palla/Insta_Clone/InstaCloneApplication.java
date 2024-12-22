ff7eb1ac6e45df4866cb878145f0d45bba4bf94f
Pallavi <ppagoti@uab.edu>
2024-12-22 16:19:26 -0600

    removed .env

--- src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
+++ src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
@@ -1,5 +1,4 @@
 package com.palla.Insta_Clone;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InstaCloneApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
+//		Dotenv dotenv = Dotenv.configure().load();
 
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("APP_API_KEY", dotenv.get("APP_API_KEY"));
+//		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+//		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//		System.setProperty("APP_API_KEY", dotenv.get("APP_API_KEY"));
 
 		SpringApplication.run(InstaCloneApplication.class, args);
 	}
