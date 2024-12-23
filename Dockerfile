6ff9e5e4208c74d56b9842aeb6a1128c5902770e
Pallavi <ppagoti@uab.edu>
2024-12-23 12:27:31 -0600

    modified dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,23 @@
-FROM openjdk:22-jdk
+FROM maven:3.9.9 AS build
 
+WORKDIR /app
 
-ADD target/instagram-clone.jar instagram-clone.jar
+COPY pom.xml .
+RUN mvn dependency:go-offline
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+FROM openjdk:22
+
+WORKDIR /app
+
+COPY --from=build /app/target/instagram-clone.jar .
+#ADD target/instagram-clone.jar instagram-clone.jar
+
+EXPOSE 8080
 
 #COPY .env .env
-ENTRYPOINT ["java","-jar","/instagram-clone.jar"]
+ENTRYPOINT ["java","-jar","/app/instagram-clone.jar"]
 
--- src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
+++ src/main/java/com/palla/Insta_Clone/InstaCloneApplication.java
@@ -1,4 +1,5 @@
 package com.palla.Insta_Clone;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ public class InstaCloneApplication {
 
 	public static void main(String[] args) {
 //		Dotenv dotenv = Dotenv.configure().load();
-
+//
 //		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
 //		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 //		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
