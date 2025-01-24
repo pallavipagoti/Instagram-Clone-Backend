2ab14ae738f0029b941e10021b74f66448ec5293
Pallavi <ppagoti@uab.edu>
2025-01-24 16:37:54 -0600

    created a controller to spin up the server quickly

--- src/main/java/com/palla/Insta_Clone/Controller/spinController.java
+++ src/main/java/com/palla/Insta_Clone/Controller/spinController.java
@@ -1,2 +1,16 @@
-package com.palla.Insta_Clone.Controller;public class spinController {
+package com.palla.Insta_Clone.Controller;
+
+import com.palla.Insta_Clone.Exceptions.UserException;
+import com.palla.Insta_Clone.Model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class spinController {
+    @GetMapping("/spin")
+    public ResponseEntity<String> spin() {
+        return new ResponseEntity<>("server started",HttpStatus.OK);
+    }
+
 }
